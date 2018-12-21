072917f3333e6f9f21b644a34c629aa3fcb5fa0c
Bryan Leighton <bryan.leigh77@gmail.com>
2018-12-21 21:17:04 +0100

    fixed and refactored tests

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 RSpec.describe BankAccount, type: :model do
 
 context "Check DB columns" do  
-	{
-		name: :string
-	}.each do |attribute, type|
-		it "should be created with #{attribute}" do 
-			is_expected.to have_db_column(attribute).of_type type
-		end
+	it "should be created with a name" do 
+		is_expected.to have_db_column(:name).of_type :string
 	end
 end
 
