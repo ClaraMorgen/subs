49d970517dd1a06440db632625baf6626e614857
Bryan Leighton <bryan.leigh77@gmail.com>
2018-12-24 01:23:05 +0100

    Revert "add category / user / bank to sub in api"
    This reverts commit d844ba0ae5696bb4120a982a7a51c4e8d0e703ce.

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::SubscriptionsController < ActionController::Base
 	def index
-		@subscriptions = Subscription.includes(:bank_account).map do |sub|
-				sub.as_json.merge({bank_account: sub.bank_account.as_json, user:sub.user.as_json, category: sub.category})
-		end
-
+		@subscriptions = Subscription.all
 		render json: @subscriptions
 	end
 end
