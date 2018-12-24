241b7e558dcc0e0da3bba8ed91197f5ef285033c
Bryan Leighton <bryan.leigh77@gmail.com>
2018-12-24 01:49:45 +0100

    allow styling to be applied

--- app/javascript/sub_pack/index.js
+++ app/javascript/sub_pack/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 //  External (Redux)
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
