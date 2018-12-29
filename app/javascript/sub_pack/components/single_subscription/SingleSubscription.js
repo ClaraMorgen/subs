99064f2dc7cdcb3df9e32d651bd6488accc53756
Clara Morgeneyer <clara_morgeneyer@web.de>
2018-12-29 20:39:18 +0100

    Cards styled

--- app/javascript/sub_pack/components/single_subscription/SingleSubscription.js
+++ app/javascript/sub_pack/components/single_subscription/SingleSubscription.js
@@ -3,15 +3,15 @@ import './SingleSubscription.scss';
 
 const SingleSubscription = (props) =>  {
 		const euros = parseFloat(props.amount / 100).toFixed(2);
-		const costEuros =  `€${euros}`;
+		const costEuros =  `€ ${euros}`;
     console.log(props)
 		return (
       <div className="single-sub">
-				<h4 className="single-sub-item sub-title">{props.title}</h4>
+				<h4 className="sub-title">{props.title}</h4>
         <div className="single-sub-item">Category</div>
-        <div className="single-sub-item">{props.endDate}</div>
-        <div className="single-sub-item">{costEuros}</div>
-		</div>
+        <div className="single-sub-item number-letter-spacing">{props.endDate}</div>
+        <div className="single-sub-item number-letter-spacing text-right">{costEuros}</div>
+      </div>
 		);
 }
 
