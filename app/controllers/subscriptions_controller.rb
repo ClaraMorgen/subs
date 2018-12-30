a56dd9da049556872bb58b0ed38e620c30aa438e
Clara Morgeneyer <clara_morgeneyer@web.de>
2018-12-31 00:21:10 +0100

    Basic styling index

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,4 +2,8 @@ class SubscriptionsController < ApplicationController
 	def index
 		@subscriptions = Subscription.all
 	end
+
+  def show
+    @subscription = Subscription.find(params[:id])
+  end
 end
