5190fb992563cc5d9b09520e071742347e259df9
Clara Morgeneyer <clara_morgeneyer@web.de>
2018-12-30 22:07:30 +0100

    Create new react route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'subscriptions#index'
-  
+
+  resources :subscriptions do
+    get 'react', on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 namespace :api, defaults: { format: :json } do
      namespace :v1 do
