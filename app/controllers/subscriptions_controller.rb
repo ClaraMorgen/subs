4cef4828eb72dbbfcd4b31efe9093dcf46b2135c
Clara Morgeneyer <clara_morgeneyer@web.de>
2018-12-31 18:42:14 +0100

    Favicon set, ajax for sub#new

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,10 +1,18 @@
 class SubscriptionsController < ApplicationController
 	def index
 		@subscriptions = Subscription.all
-    @subscription = Subscription.new
-	end
+  end
 
   def show
     @subscription = Subscription.find(params[:id])
+  end
+
+  def new
+    @subscription = Subscription.new
+
+    respond_to do |format|
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.js  # <-- will render `app/views/subscription/new.js.erb`
+      end
   end
 end
