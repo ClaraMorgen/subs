05f716fff1a58360326bdd3d3f6763ee7e013ab6
Clara Morgeneyer <clara_morgeneyer@web.de>
2019-01-01 22:21:05 +0100

    NO filter but close button on form, create works

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,10 +9,23 @@ class SubscriptionsController < ApplicationController
 
   def new
     @subscription = Subscription.new
-
     respond_to do |format|
-        format.html { redirect_to restaurant_path(@restaurant) }
-        format.js  # <-- will render `app/views/subscription/new.js.erb`
-      end
+      format.html { redirect_to subscriptions_path }
+      format.js  # <-- will render `app/views/subscription/new.js.erb`
+    end
+  end
+
+  def create
+    @subscription = Subscription.new(subscription_params)
+    @subscription.user = current_user
+    @subscription.save
+    flash[:notice] = "You have successfully created a subscription."
+    redirect_to root_path
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :due_date, :end_date, :amount_cents, :category_id, :bank_account_id)
   end
 end
