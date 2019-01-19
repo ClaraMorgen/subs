2a111ae44d16a0cabac9fba4615d818944f7f37b
Clara Morgeneyer <clara_morgeneyer@web.de>
2019-01-19 15:28:12 +0100

    Add logic so devis gets other layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
